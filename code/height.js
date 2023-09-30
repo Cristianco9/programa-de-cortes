ec3b7d72c4b22090638a3b89c7c60f34d6372206
Cristian Camilo <cristian_cortes_ortiz@hotmail.com>
2023-09-29 22:10:35 -0500

    funciones altura y anchura creadas

---
+++ code/height.js
@@ -0,0 +1,12 @@
+// let inputEight = document.getElementById('').value;
+let inputEight = 60;
+// const eightDsc = eightDscMeasure;
+const eightDsc = 4;
+
+function calculateEight(inputEight){
+    let outputEight = inputEight - eightDsc - eightDsc;
+    return outputEight;
+};
+
+// calculateEight();
+console.log("the Eight is", calculateEight());
