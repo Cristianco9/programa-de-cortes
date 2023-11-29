2fb3622dffded79d45e08288be81a7b0e8ab64c1
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2023-11-29 00:29:49 -0500

    heavyweight functions created

---
+++ src/js-functions/discount-functions/heavy-wieght-discount/discount-values.js
@@ -0,0 +1,11 @@
+const widthDscMeasure = 7.2;
+const heightDscMeasure = 11.8;
+const fixedWidthDscMeasure = 7.2;
+const fixedHeightDscMeasure = 7.2;
+
+module.exports = {
+    widthDscMeasure,
+    heightDscMeasure,
+    fixedWidthDscMeasure,
+    fixedHeightDscMeasure
+};
