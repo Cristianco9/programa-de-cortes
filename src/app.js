7840d818ae32a869a3454c304937cf3928b781bb
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2023-12-28 21:46:13 -0500

    server created with express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log("running");
+import express from "express";
+import path from "path";
+import morgan from "morgan";
+
+import userRoutes from "./routes/user.routes.js";
+import { fileURLToPath } from "url";
+
+// create the app with express
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+// select routes
+app.use(customerRoutes);
+
+// static files path
+app.use(express.static(path.join(__dirname, "public")));
+
+// starting the server
+export default app;
