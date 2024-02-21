db51915c74cde05e25527d5ce6fdf58cb11276ff
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-02-20 22:59:29 -0500

    environment varibles implemented PRETTY SOFT

--- src/libraries/DBConnection.js
+++ src/libraries/DBConnection.js
@@ -1,22 +1,28 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
+import { config } from "../config/config.js";
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 export const getConnection = async () => {
-  const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'developer',
-    password: '1234567890',
-    database: 'instalando_cali_db'
-  });
+  const pool = new Pool({ connectionString: URI });
   return pool;
 };
 
 export const testConnection = async () => {
   try {
-    const client = await getConnection();
-    console.log('Database connected!');
-    return true;
+    const pool = await getConnection();
+
+    const client = await pool.connect();
+    const result = await client.query('SELECT NOW()');
+
+    console.log('Database connected:', result.rows[0].now);
+    client.release();
+
   } catch (err) {
     console.error('Error connecting to the database:', err.message);
     return false;
