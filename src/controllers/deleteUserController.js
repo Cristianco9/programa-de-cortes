f94564804bc55ec0806e96ba6c26a4f71d6d6d38
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-03-20 03:17:00 -0500

    delete user end-point completed

--- src/controllers/deleteUserController.js
+++ src/controllers/deleteUserController.js
@@ -2,5 +2,39 @@ import { User } from '../db/models/userModel.js';
 import Boom from '@hapi/boom';
 
 export const deleteUser = async (req, res, next) => {
+  const { id } = req.params;
 
+  try {
+    const deleteRecord = await User.destroy({
+      where: {
+        id: id
+      }
+    });
+
+    try {
+      const result = await User.findAll({
+        attributes: ['id', 'rol', 'name' ],
+        order: [['id', 'ASC']]
+      });
+
+      const usersCreated = result;
+      const usersCreatedQuantity = usersCreated.length;
+
+      res.status(200).json({
+        usersCreatedQuantity: usersCreatedQuantity,
+        usersCreated: usersCreated
+      })
+    } catch (err) {
+        const boomError = Boom.serverUnavailable(
+          'No es posible listar los usuarios creados',
+          err);
+        next(boomError);
+    };
+
+  } catch (err) {
+    const boomError = Boom.serverUnavailable(
+      'No es posible eliminar el usuario de la base de datos',
+      err);
+    next(boomError);
+  };
 }
