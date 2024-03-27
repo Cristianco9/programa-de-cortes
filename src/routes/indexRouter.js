144339d567fc3c5dff8b0c906aae6d9a46dff02a
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-03-27 13:45:24 -0500

    router refactorized to simplificate

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -4,20 +4,6 @@ import toolsRouter from "./toolsRouter.js";
 import ordersRouter from "./ordersRouter.js";
 import ordersIDRouter from "./ordersIDRouter.js";
 import ordersActionsRouter from "./ordersActionsRouter.js";
-import typeRouter from "./typeRouter.js";
-import typeToDuplicateRouter from "./typeToDuplicateRouter.js";
-import formLightRouter from "./formLightRouter.js";
-import formHeavyRouter from "./formHeavyRouter.js";
-import createAnjeoLightRouter from "./createAnjeoLightRouter.js";
-import createAnjeoHeavyRouter from "./createAnjeoHeavyRouter.js";
-import listAnjeosLightRouter from "./listAnjeosLightRouter.js";
-import listAnjeosHeavyRouter from "./listAnjeosHeavyRouter.js";
-import editAnjeoLightRouter from "./editAnjeoLightRouter.js";
-import editAnjeoHeavyRouter from "./editAnjeoHeavyRouter.js";
-import deleteAnjeoLightRouter from "./deleteAnjeoLightRouter.js";
-import deleteAnjeoHeavyRouter from "./deleteAnjeoHeavyRouter.js";
-import updateFormLightRouter from "./updateFormLightRouter.js";
-import updateFormHeavyRouter from "./updateFormHeavyRouter.js";
 import settingsRouter from './settingsRouter.js';
 
 const routerApi = (app) => {
@@ -31,20 +17,6 @@ const routerApi = (app) => {
   router.use('/order', ordersRouter);
   router.use('/orderID', ordersIDRouter);
   router.use('/orderActions', ordersActionsRouter);
-  router.use('/type', typeRouter);
-  router.use('/typeToDuplicate', typeToDuplicateRouter);
-  router.use('/formLight', formLightRouter);
-  router.use('/formHeavy', formHeavyRouter);
-  router.use('/createFormLight', createAnjeoLightRouter);
-  router.use('/createFormHeavy', createAnjeoHeavyRouter);
-  router.use('/listLight', listAnjeosLightRouter);
-  router.use('/listHeavy', listAnjeosHeavyRouter);
-  router.use('/editAnjeoLight', editAnjeoLightRouter);
-  router.use('/editAnjeoHeavy', editAnjeoHeavyRouter);
-  router.use('/deleteAnjeoLight', deleteAnjeoLightRouter);
-  router.use('/deleteAnjeoHeavy', deleteAnjeoHeavyRouter);
-  router.use('/updateFormLight', updateFormLightRouter);
-  router.use('/updateFormHeavy', updateFormHeavyRouter);
   router.use('/settings', settingsRouter);
 
 }
--- src/routes/ordersActionsRouter.js
+++ src/routes/ordersActionsRouter.js
@@ -2,13 +2,27 @@ import { Router } from "express";
 import { verifyToken } from '../middlewares/tokenHandler.js';
 import { checkRole } from '../middlewares/checkRoleHandler.js';
 import { orderActions } from "../controllers/orderActionsController.js";
+import { type } from "../controllers/typeController.js";
+import { formLight } from "../controllers/formLightController.js";
+import { createAnejoLight } from "../controllers/createAnjeoLightController.js";
+import { createAnjeoHeavy } from "../controllers/createAnjeoHeavyController.js";
+import { formHeavy } from "../controllers/formHeavyController.js";
 import { typeCreated } from "../controllers/typeCreatedController.js";
+import { typeToDuplicate } from "../controllers/typeToDuplicateController.js";
+import { listAnjeosLight } from "../controllers/listAnjeosLightController.js";
+import { listAnjeosHeavy } from "../controllers/listAnjeosHeavyController.js";
 import { duplicateAnjeoLight } from "../controllers/duplicateAnjeoLightController.js";
 import { duplicateAnjeoHeavy } from "../controllers/duplicateAnjeosHeavyController.js";
 import { validatorHandler } from '../middlewares/validatorHandler.js';
 import { anjeosLightSchema } from '../schemas/anjeoLightSchema.js';
 import { anjeosHeavySchema } from '../schemas/anjeoHeavySchema.js';
-import { recordLightDuplicate } from "../controllers/recordLigthDuplicate.js";
+import { editAnjeoLight } from "../controllers/editAnjeosLightController.js";
+import { editAnjeoHeavy } from "../controllers/editAnjeosHeavyController.js";
+import { updateAnjeoLight } from "../controllers/updateAnjeoLightController.js";
+import { updateAnjeoHeavy } from "../controllers/updateAnjeoHeavyController.js";
+import { deleteAnjeoLight } from "../controllers/deleteAnjeoLightController.js";
+import { deleteAnjeoHeavy } from "../controllers/deleteAnjeoHeavyController.js";
+import { recordLightDuplicate } from "../controllers/recordLightDuplicate.js";
 import { recordHeavyDuplicate } from "../controllers/recordHeavyDuplicate.js";
 
 const router = Router();
@@ -21,10 +35,112 @@ router.get(
 );
 
 router.get(
+  '/type',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  type
+);
+
+router.get(
+  '/type/formLight',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  formLight
+);
+
+router.post(
+  '/type/formLight/createFormLight',
+  validatorHandler(anjeosLightSchema, 'body'),
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  createAnejoLight
+);
+
+router.get(
+  '/type/formHeavy',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  formHeavy
+);
+
+router.post(
+  '/type/formHeavy/createFormHeavy',
+  validatorHandler(anjeosHeavySchema, 'body'),
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  createAnjeoHeavy
+);
+
+router.get(
   '/typeCreated',
   verifyToken,
   checkRole(['administrador', 'asesor']),
   typeCreated
+);
+
+router.get(
+  '/listLight',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  listAnjeosLight
+);
+
+router.get(
+  '/listHeavy',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  listAnjeosHeavy
+);
+
+router.get(
+  '/editAnjeoLight/:id',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  editAnjeoLight
+);
+
+router.post(
+  '/updateFormLight/:id',
+  validatorHandler(anjeosLightSchema, 'body'),
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  updateAnjeoLight
+);
+
+router.get(
+  '/editAnjeoHeavy/:id',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  editAnjeoHeavy
+);
+
+router.post(
+  '/updateFormHeavy/:id',
+  validatorHandler(anjeosHeavySchema, 'body'),
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  updateAnjeoHeavy
+);
+
+router.get(
+  '/deleteAnjeoLight/:id',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  deleteAnjeoLight
+);
+
+router.get(
+  '/deleteAnjeoHeavy/:id',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  deleteAnjeoHeavy
+);
+
+router.get(
+  '/typeToDuplicate',
+  verifyToken,
+  checkRole(['administrador', 'asesor']),
+  typeToDuplicate
 );
 
 router.get(
